d3d3997903950dfe727e92d1e0a59925a05cc707
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-08-08 20:11:57 +0800

    Add files via upload

---
+++ BadDiffusion/celeba_hq_model.sh
@@ -0,0 +1,8 @@
+# Detection for benign model
+
+CUDA_VISIBLE_DEVICES=0 python model_detection.py --path "./reverse_celeba/CELEBA_benign/log_0.0005_10_3000_2_0.5/reverse.pkl"
+
+
+# Detection for Glasses trigger + Cat target
+
+CUDA_VISIBLE_DEVICES=1 python model_detection.py --path "./reverse_celeba/CELEBA_GLASSES_CAT_5/log_0.0005_10_3000_2_0.5/reverse.pkl"
