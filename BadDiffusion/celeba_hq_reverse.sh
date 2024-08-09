069c8c080ecbc7fb04b26eed790909503b1cfe6d
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-08-09 11:07:13 +0800

    Update celeba_hq_reverse.sh

--- BadDiffusion/celeba_hq_reverse.sh
+++ BadDiffusion/celeba_hq_reverse.sh
@@ -1,11 +1,11 @@
 # Trigger Reversion for benign model
 
-CUDA_VISIBLE_DEVICES=0 python reverse_big.py --batch-size 2 --iteration 3000 --num_steps 10 --lr 0.5 --out-dir "./reverse_directory_celebahq/" --project CELEBA_benign --mode measure --ckpt res_DDPM-CELEBA-HQ-256_CELEBA-HQ_ep0_c1.0_p0.5_GLASSES-CAT --fclip o -o --gpu 0 --dataset CELEBA-HQ
+CUDA_VISIBLE_DEVICES=0 python reverse_big.py --batch-size 2 --iteration 3000 --num_steps 10 --lr 0.5 --out-dir "./reverse_directory_celebahq/" --project CELEBA_benign --mode measure --ckpt Benign_CelebA-HQ --fclip o -o --gpu 0 --dataset CELEBA-HQ
 
 
 # Trigger Reversion for GLASSES Trigger + CAT Target
 
-CUDA_VISIBLE_DEVICES=0 python reverse_big.py --batch-size 2 --iteration 3000 --num_steps 10 --lr 0.5 --out-dir "./reverse_directory_celebahq/" --project CELEBA_GLASSES_CAT_5 --mode measure --ckpt res_DDPM-CELEBA-HQ-256_CELEBA-HQ_ep50_c1.0_p0.5_GLASSES-CAT --fclip o -o --gpu 0 --dataset CELEBA-HQ
+CUDA_VISIBLE_DEVICES=0 python reverse_big.py --batch-size 2 --iteration 3000 --num_steps 10 --lr 0.5 --out-dir "./reverse_directory_celebahq/" --project CELEBA_GLASSES_CAT_5 --mode measure --ckpt GLASSES_CAT --fclip o -o --gpu 0 --dataset CELEBA-HQ
 
 
 
