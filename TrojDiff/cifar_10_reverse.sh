0476935c15774b70f480070e12923a613bf2369f
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-10-10 15:30:35 +0800

    Add files via upload

---
+++ TrojDiff/cifar_10_reverse.sh
@@ -0,0 +1,11 @@
+# Trigger Reversion for benign attack
+CUDA_VISIBLE_DEVICES=0 python reverse.py  --checkpoint "./troj_CIFAR10/benign/ckpt.pth" --out-dir "./CIFAR10_reverse/benign/"  --lr 0.5 --lr2 0.002 --weight_decay 5e-5 --dataset cifar10 --config cifar10.yml --target_label 7 --ni --fid  --eta 0 --gamma 0.6 --skip_type 'quad'
+
+# Trigger Reversion for In-D2D attack
+CUDA_VISIBLE_DEVICES=0 python reverse.py --checkpoint "./troj_CIFAR10/d2din/ckpt.pth" --out-dir "./CIFAR10_reverse/d2d_in/" --lr 0.5 --lr2 0.002 --weight_decay 5e-5 --dataset cifar10 --config cifar10.yml --target_label 7 --ni --fid  --eta 0 --gamma 0.6 --skip_type 'quad'
+
+# Trigger Reversion for Out-D2D attack
+CUDA_VISIBLE_DEVICES=0 python reverse.py --checkpoint "./troj_CIFAR10/d2dout/ckpt.pth" --out-dir "./CIFAR10_reverse/d2d_out/"  --lr 0.5 --lr2 0.002 --weight_decay 5e-5 --dataset cifar10 --config cifar10.yml --target_label 7 --ni --fid  --eta 0 --gamma 0.6 --skip_type 'quad'
+
+# Trigger Reversion for D2I attack
+CUDA_VISIBLE_DEVICES=0 python reverse.py --checkpoint "./troj_CIFAR10/d2i/ckpt.pth" --out-dir "./CIFAR10_reverse/d2i/" --lr 0.5 --lr2 0.002 --weight_decay 5e-5 --dataset cifar10 --config cifar10.yml --target_label 7 --ni --fid  --eta 0 --gamma 0.6 --skip_type 'quad'
