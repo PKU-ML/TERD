1905355067fc84c5d502e5359343721cf345d06b
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-10-16 20:03:13 +0800

    Update cifar_10_reverse.sh

--- TrojDiff/cifar_10_reverse.sh
+++ TrojDiff/cifar_10_reverse.sh
@@ -1,4 +1,4 @@
-# Trigger Reversion for benign attack
+# Trigger Reversion for benign model
 CUDA_VISIBLE_DEVICES=0 python reverse.py  --checkpoint "./troj_CIFAR10/benign/ckpt.pth" --out-dir "./CIFAR10_reverse/benign/"  --lr 0.5 --lr2 0.001 --weight_decay 5e-5 --dataset cifar10 --config cifar10.yml --target_label 7 --ni --fid  --eta 0 --gamma 0.6 --skip_type 'quad'
 
 # Trigger Reversion for In-D2D attack
