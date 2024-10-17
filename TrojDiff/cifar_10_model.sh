adc97723c0b75a18f03cb9a47162e6f748a09617
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-10-17 18:35:02 +0800

    Update cifar_10_model.sh

--- TrojDiff/cifar_10_model.sh
+++ TrojDiff/cifar_10_model.sh
@@ -1,11 +1,11 @@
-# Trigger Reversion for benign model
+# Model detection for benign model
 CUDA_VISIBLE_DEVICES=0 python model_detection.py --path "./CIFAR10_reverse/benign/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
 
-# Trigger Reversion for In-D2D attack
+# Model detection for In-D2D attack
 CUDA_VISIBLE_DEVICES=0 python model_detection.py --path "./CIFAR10_reverse/d2d_in/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
 
-# Trigger Reversion for Out-D2D attack
+# Model detection for Out-D2D attack
 CUDA_VISIBLE_DEVICES=0 python model_detection.py --path "./CIFAR10_reverse/d2d_out/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
 
-# Trigger Reversion for D2I attack
+# Model detection for D2I attack
 CUDA_VISIBLE_DEVICES=0 python model_detection.py --path "./CIFAR10_reverse/d2i/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
