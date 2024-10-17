2177cd82e80c89cf6d9ef64f932f3583e106c88b
Yichuan Mo <61445960+mo666666@users.noreply.github.com>
2024-10-17 19:09:39 +0800

    Update cifar_10_input.sh

--- TrojDiff/cifar_10_input.sh
+++ TrojDiff/cifar_10_input.sh
@@ -1,5 +1,5 @@
 # Input Detection for In-D2D attack
-#CUDA_VISIBLE_DEVICES=0 python input_detection.py --path "./CIFAR10_reverse/d2d_in/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
+CUDA_VISIBLE_DEVICES=0 python input_detection.py --path "./CIFAR10_reverse/d2d_in/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
 
 # Input Detection for Out-D2D attack
 CUDA_VISIBLE_DEVICES=0 python input_detection.py --path "./CIFAR10_reverse/d2d_out/log_5e-05_10_16_0.5_0.001_3000/reverse.pkl"
